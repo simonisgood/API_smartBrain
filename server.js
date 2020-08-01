e34a7f248688cc10d2be2ecc7376b26eec6618d0
simonisgood <a0938964930@gmail.com>
2020-08-01 16:09:25 +0800

    connect_heroku_v3

--- server.js
+++ server.js
@@ -31,7 +31,7 @@ app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, db) }
 app.put('/image', (req, res) => { image.handleImage(req, res, db) })
 app.post('/imageurl', (req, res) => { image.handleApiCall(req, res) })
 
-
-app.listen(process.env.PORT || 3000, ()=> {
+const PORT = process.env.PORT
+app.listen(PORT || 3000, ()=> {
 	console.log(`app is running on port ${PORT}`)
 })
