1c326b7262b38c227423c1f1dfc62f3ede2a7685
simonisgood <a0938964930@gmail.com>
2020-08-01 17:37:32 +0800

    upload_DB_v1

--- server.js
+++ server.js
@@ -14,13 +14,13 @@ const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
+const PORT = process.env.PORT
+
 const db = require('knex')({
   client: 'pg',
   connection: {
-    host : '127.0.0.1',
-    user : 'postgres',
-    password : 'simon0938964930',
-    database : 'smart_brain'
+    connectionString : PORT,
+    ssl: true
   }
 });
 
@@ -31,7 +31,6 @@ app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, db) }
 app.put('/image', (req, res) => { image.handleImage(req, res, db) })
 app.post('/imageurl', (req, res) => { image.handleApiCall(req, res) })
 
-const PORT = process.env.PORT
 app.listen(PORT || 3000, ()=> {
 	console.log(`app is running on port ${PORT}`)
 })
